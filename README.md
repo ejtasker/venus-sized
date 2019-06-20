9fbc4bddd0854db0b3e1f799f5fa68e76757e5b5
ejtasker <30892208+ejtasker@users.noreply.github.com>
2019-06-20 11:38:32 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Chrome extension that replaces "Earth-sized" with "Venus-sized" to remind you that size alone does not make a habitable world. 
-
+Chrome extension that replaces "Earth-sized" and "Earth-like" with "Venus-sized" and "Venus-like" to remind you that size alone does not make a habitable world.
 To install the app:
 (1) Go to chrome://extentions in your browser
 (2) Flick on "Developer mode" in the top right
--- content.js
+++ content.js
@@ -8,7 +8,9 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Earth-sized/gi, 'Venus-sized');
+            var replacedText =
+              text.replace(/Earth-sized/gi, 'Venus-sized')
+              .replace(/Earth-like/gi, 'Venus-like');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
